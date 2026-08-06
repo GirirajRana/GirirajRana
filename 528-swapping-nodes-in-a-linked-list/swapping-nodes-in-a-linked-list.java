5a240867fe15db210d1f14859da32d275df4f142
Giriraj <girirajrana558@gmail.com>
2026-08-06 16:50:36 +0530

    Time: 2 ms (52.77%) | Memory: 119.2 MB (46.00%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -8,30 +8,67 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ //one pass solution preferred
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-     int len=0;
-     ListNode temp=head;
-     while(temp!=null){
-        temp=temp.next;
-        len++;
-     }
+        ListNode slow=head;
+        // ListNode fast=head;
+        ListNode temp=head;
+        int len=0;
+        while(temp!=null){
+            temp=temp.next;
+            len++;
+        }
+        temp=head;
 
-    ListNode first=head;
-     for(int i=1;i<=k-1;i++){
-        first=first.next;
-     }
+        for(int i=1;i<k;i++){
+            slow=slow.next;
+        }
 
+        ListNode fast=slow;
 
-    ListNode second=head;
-     for(int i=1;i<=len-k;i++){
-        second=second.next;
-     }
+        while(fast.next!=null){
+            fast=fast.next;
+            temp=temp.next;
+        }
 
-     int tempVal=first.val;
-     first.val=second.val;
-     second.val=tempVal;
+        int tempVal=temp.val;
+        temp.val=slow.val;
+        slow.val=tempVal;
 
-     return head;
+        return head;
     }
 }
+
+
+
+
+//Two pass solution
+// class Solution {
+//     public ListNode swapNodes(ListNode head, int k) {
+//      int len=0;
+//      ListNode temp=head;
+//      while(temp!=null){
+//         temp=temp.next;
+//         len++;
+//      }
+
+//     ListNode first=head;
+//      for(int i=1;i<=k-1;i++){
+//         first=first.next;
+//      }
+
+
+//     ListNode second=head;
+//      for(int i=1;i<=len-k;i++){
+//         second=second.next;
+//      }
+
+//      int tempVal=first.val;
+//      first.val=second.val;
+//      second.val=tempVal;
+
+//      return head;
+//     }
+// }
