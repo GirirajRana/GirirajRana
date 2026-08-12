ec540da98d5669ffc51ff38724ff88d484b92b5a
Giriraj <girirajrana558@gmail.com>
2026-08-12 17:39:06 +0530

    Time: 2 ms (57.78%) | Memory: 46.7 MB (29.39%) - LeetSync

---
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseLinkedList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode Next=head;//or head/null both valid
+        while(curr!=null){
+            Next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=Next;
+        }
+        return prev;
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1=reverseLinkedList(l1);
+        l2=reverseLinkedList(l2);
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        int carry =0;
+        while(l1!=null || l2!=null){
+            int val1=0,val2=0;
+            if(l1!=null) val1=l1.val;
+            if(l2!=null) val2=l2.val;
+            int num=val1+val2+carry;
+            ListNode node=new ListNode(num%10);
+            temp.next=node;
+            temp=temp.next;
+            if(num>9) carry=1;
+            else carry=0;
+            if(l1!=null)  l1=l1.next;
+            if(l2!=null)  l2=l2.next;
+        }
+         if(carry==1){
+            ListNode node=new ListNode(1);
+            temp.next=node;
+            temp=temp.next;
+        }
+        return reverseLinkedList(dummy.next);
+    }
+}
